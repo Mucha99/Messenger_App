0acad750cbe022abc7149f8dd44b20f45e52aeb8
Bartek Mucha <bartekmucha43@gmail.com>
2021-07-20 19:54:38 +0200

    Add client - function

---
+++ client/app.js
@@ -0,0 +1,50 @@
+const select = {
+    loginForm: document.getElementById('welcome-form'),
+    messagesSection: document.getElementById('messages-section'),
+    messagesList: document.getElementById('messages-list'),
+    addMessageForm: document.getElementById('add-messages-form'),
+    userNameInput: document.getElementById('username'),
+    messageContentInput: document.getElementById('message-content'),
+};
+
+let userName;
+
+const login = function(event) {
+    event.preventDefault();
+    if(select.userNameInput.value){
+        userName = select.userNameInput.value;
+        select.loginForm.classList.remove('show');
+        select.messagesSection.classList.add('show');
+    } else {
+        alert('Login error... Try again!');
+    }
+}
+
+function addMessage (author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) {
+        message.classList.add('message--self');
+        message.innerHTML = `
+            <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+            <div class="message__content">
+                ${content}
+            </div>
+        `;
+        select.messagesList.appendChild(message);
+    }
+}
+
+const sendMessage = function(event) {
+    event.preventDefault();
+    if(select.messageContentInput.value) {
+        addMessage(userName, select.messageContentInput.value);
+        select.messageContentInput.value = '';
+    } else {
+        alert('Try again!')
+    }
+}
+
+select.loginForm.addEventListener('submit', login);
+select.addMessageForm.addEventListener('submit', sendMessage);
